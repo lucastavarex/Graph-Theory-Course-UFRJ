792a34f6d9fe7eed8572033c2f9070c20b1cf8b4
Lucas Tavares <lucastavares@pop-os.localdomain>
2023-09-26 18:34:15 -0300

    Adicionando tags de otm + fixes

--- setup.py
+++ setup.py
@@ -82,8 +82,8 @@ def cpp_flag(compiler):
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
     c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
+        'msvc': ['/EHsc', '-O3'],
+        'unix': ['-O3'],
     }
 
     if sys.platform == 'darwin':
